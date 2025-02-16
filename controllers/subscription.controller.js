1f13eb0c6ab9800ac87f3ee79076d84ad2d29a7c
Movin Tilakaratne <movin1999t@gmail.com>
2025-02-16 15:47:00 +0530

    Create reminder workflow

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,6 @@
+import { workflowClient } from '../config/upstash.js';
 import Subscription from '../models/subscription.model.js';
+import { SERVER_URL } from '../config/env.js';
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -7,10 +9,20 @@ export const createSubscription = async (req, res, next) => {
       user: req.user._id,
     });
 
-    res.status(201).json({
-      succuess: true,
-      data: subscription,
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        'content-type': 'application/json',
+      },
+      retries: 0,
     });
+
+    res
+      .status(201)
+      .json({ success: true, data: { subscription, workflowRunId } });
   } catch (error) {
     next(error);
   }
@@ -28,7 +40,7 @@ export const getUserSubscriptions = async (req, res, next) => {
     const subscriptions = await Subscription.find({ user: req.params.id });
 
     res.status(200).json({
-      succuess: true,
+      success: true,
       data: subscriptions,
     });
   } catch (error) {
